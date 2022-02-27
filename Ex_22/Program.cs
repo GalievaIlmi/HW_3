19e20492adb10c2de2de0f520662b78570989f7e
Ilmira <gal.ilmir@mail.ru>
2022-02-27 16:10:33 +0300

    add Ex_22
    :
    :wq

---
+++ Ex_22/Program.cs
@@ -0,0 +1,16 @@
+﻿// // Задача 22: Найти расстояние между точками в пространстве 2D/3D
+// В пространстве 2D S(расстояние м/у точуами)  = (X2-X1)^2 + (Y2-Y1)^2 
+Console.WriteLine("расположение первой точки по оси x:");
+int A1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("расположение первой точки по оси y:");
+int B1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("расположение второй точки по оси x:");
+int A2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("расположение второй точки по оси y:");
+int B2 = int.Parse(Console.ReadLine());
+double distance = ((A1 - A2) * (A1 - A2)) + ((B1 - B2) * (B1 - B2));
+
+Console.WriteLine($"Расстояние: {Math.Sqrt(distance)}");
